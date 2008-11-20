bd2c0b03d3a857c1ca76d3779f50d8981caecb7b
Evan Wheeler <evanmwheeler@gmail.com>
2008-11-20 16:05:08 -0500

    more models

--- webui/poll/models.py
+++ webui/poll/models.py
@@ -9,11 +9,14 @@ class Respondant(models.Model):
 	phone = models.CharField(max_length=30, blank=True, null=True)
 	is_active = models.BooleanField()
 
+	def __unicode__(self):
+		return self.phone
+
 
 class Message(models.Model):
 	respondant = models.ForeignKey(Respondant, blank=True, null=True)
 	time = models.DateTimeField(auto_now_add=True)
-	message = models.CharField(max_length=160)
+	text = models.CharField(max_length=160)
 	is_outgoing = models.BooleanField()
 
 	# todo: what is this for? the screen log?
@@ -21,3 +24,31 @@ class Message(models.Model):
 		if self.is_outgoing: dir = ">>"
 		else:                dir = "<<"
 		return "%s %s: %s" % (dir, self.respondant, self.message)
+
+
+class Question(models.Model):
+	QUESTION_TYPES = (
+		('F', 'Free text'),
+		('B', 'Boolean'),
+		('M', 'Multiple choice'),
+	)
+
+	start = models.DateTimeField()
+	end = models.DateTimeField()
+	text = models.CharField(max_length=160)
+	type = models.CharField(max_length=1, choices=QUESTION_TYPES)
+
+	def __unicode__(self):
+		return self.text
+
+
+class Response(models.Model):
+	respondant = models.ForeignKey(Respondant, blank=True, null=True)
+	question = models.ForeignKey(Question, blank=True, null=True)
+	message = models.ForeignKey(Message, blank=True, null=True)
+	time = models.DateTimeField(auto_now_add=True)
+	text = models.CharField(max_length=160)
+	is_unparseable = models.BooleanField()
+
+	def __unicode__(self):
+		return self.text
