4311fc98ee9481df83097407ff73dcd6c577fe08
Evan Wheeler <evanmwheeler@gmail.com>
2008-11-20 17:44:34 -0500

    strings

---
+++ strings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# vim: noet
+
+
+ENGLISH = {
+	"conv_welc": "You're welcome, %s!",
+	"conv_greet": "Greetings, friend!",
+	"conv_swear": "Would you text that to your mother, %s?"
+}
