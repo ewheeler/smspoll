996e4e2f834e39a6a46488040d5aed7729b46d42
Mark E Johnston <mejymejy@gmail.com>
2008-11-21 18:59:18 -0500

    added admin file

---
+++ webui/poll/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin 
+from webui.poll.models import *
+
+admin.site.register(Respondant)
+admin.site.register(Message)
+admin.site.register(Question)
+admin.site.register(Response)
+
