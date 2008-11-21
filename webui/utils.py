1a4ab7d0825661d22c8309dfb3f7fc0b29e4f991
Evan Wheeler <evanmwheeler@gmail.com>
2008-11-21 12:53:50 -0500

    utils file with blast_numbers

---
+++ webui/utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim: noet
+
+import kannel
+from smsapp import *
+
+def blast_numbers(numbers, message):
+	# blasts a message to a list of numbers
+	sending = 0
+	sender = kannel.SmsSender("user", "password")
+	for n in numbers:
+		sender.send(n, message)
+		sending += 1
+		print 'Blasted to %d of %d numbers...' % (sending, len(numbers))
+        return 'Blasted %s to %d numbers with %d failures' % (message, sending, (len(numbers) - sending))
