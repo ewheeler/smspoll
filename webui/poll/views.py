031c85a32e2b1aae3ee7d556cf4278669a1b59c4
Evan Wheeler <evanmwheeler@gmail.com>
2008-12-01 11:35:32 -0500

    more comments

--- webui/poll/views.py
+++ webui/poll/views.py
@@ -24,12 +24,13 @@ def dashboard(req, id=None):
 	else: ques = get_object_or_404(Question, pk=id)
 	
 	# the previous questions are always the same (for
-	# now); todo: show those adjacent to 'ques'
+	# now); TODO: show those adjacent to 'ques'
 	prev = Question.objects.all()[:12]
 	
 	# show all of the answers related to this
 	# question. these have already been filtered
 	# by the backend, but not moderated
+	# TODO: this bombs if there is no active question
 	entries = ques.entry_set.all()
 	
 	return render_to_response("dashboard.html", {
