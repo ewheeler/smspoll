4285d401719fff20c5376317629147d834c261bb
Mark E Johnston <mejymejy@gmail.com>
2008-12-02 16:33:55 -0500

    updated parsing of messages so it can be used for unparseables / moved from backend.py to webui/utils.py

--- backend.py
+++ backend.py
@@ -5,7 +5,6 @@ import kannel
 from smsapp import *
 from datetime import date, datetime
 from strings import ENGLISH as STR
-import re
 
 # import the essentials of django
 from django.core.management import setup_environ
@@ -16,16 +15,7 @@ setup_environ(settings)
 # import the django models, which should be movd
 # somewhere sensible at the earliest opportunity
 from webui.poll.models import *
-
-# regexes for matching boolean anwsers
-B_REGEX_TRUE  = re.compile(r'^yes$', re.I)
-B_REGEX_FALSE = re.compile(r'^no$', re.I)
-
-# regexes for matching multiple choice answers
-M_REGEX_1 = re.compile(r'^1$', re.I)
-M_REGEX_2 = re.compile(r'^2$', re.I)
-M_REGEX_3 = re.compile(r'^3$', re.I)
-M_REGEX_4 = re.compile(r'^4$', re.I)
+from webui import utils
 
 
 class App(SmsApplication):
@@ -79,87 +69,12 @@ class App(SmsApplication):
 		ques = Question.current()
 		if ques is None: self.respond(STR["no_question"])
 		
-		# if we are logging free text answers,
-		# just move the message straight into
-		# the entries (unmoderated!)
-		if ques.type == "F":
-			Entry.objects.create(
-				respondant=r,
-				question=ques,
-				message=self.log_msg,
-				is_unparseable=False,
-				moderated=False,
-				text=msg)
-			self.respond(STR["thanks"])
+		# try to parse the message
+		parsed = utils.parse_message(self.log_msg, ques)
 
-		# if we are logging boolean answers,
-		# then we need to parse them based
-		# on yes/no regexes
-		if ques.type == "B":
-			# assume the message is unparseable
-			unparseable = True
-			text = msg
-			response = STR["thanks_unparseable"]
-			
-			# if yes matches, make a '1' entry
-			if B_REGEX_TRUE.match(msg):
-				text = '1'
-				unparseable = False
-				response = STR["thanks"]
-			
-			# if no matches, make a '0' entry
-			elif B_REGEX_FALSE.match(msg):
-				text = '0'
-				unparseable = False
-				response = STR["thanks"]
-
-			Entry.objects.create(
-				respondant=r,
-				question=ques,
-				message=self.log_msg,
-				is_unparseable=True,
-				moderated=True,
-				text=msg)
-			self.respond(response)
-
-		# if we are logging multiple choice,
-		# answers, then we need to parse
-		# them based on our mc regexes
-		if ques.type == "M":
-			# assume the message is unparseable
-			unparseable = True
-			text = msg
-			response = STR["thanks_unparseable"]
-
-			# if 1 matches, make the entry
-			if M_REGEX_1.match(msg):
-				text = '1'
-				unparseable = False
-				response = STR["thanks"]
-			# 2 matches...
-			elif M_REGEX_2.match(msg):
-				text = '2'
-				unparseable = False
-				response = STR["thanks"]
-			# 3 matches...
-			elif M_REGEX_3.match(msg):
-				text = '3'
-				unparseable = False
-				response = STR["thanks"]
-			# 4 matches...
-			elif M_REGEX_4.match(msg):
-				text = '4'
-				unparseable = False
-				response = STR["thanks"]
-			
-			Entry.objects.create(
-				respondant=r,
-				question=ques,
-				message=self.log_msg,
-				is_unparseable=True,
-				moderated=True,
-				text=msg)
-			self.respond(response)
+		# send an appropriate response to the caller
+		if parsed:	self.respond(STR["thanks"])
+		else:       self.respond(STR["thanks_unparseable"])
 
 
 	# LOGGING -----------------------------------------------------------------
@@ -175,9 +90,7 @@ class App(SmsApplication):
 			phone=caller,
 			text=msg)
 		
-		
-	
-	
+
 	# as above...
 	def before_outgoing(self, caller, msg):
 		
@@ -188,6 +101,7 @@ class App(SmsApplication):
 			text=msg)
 
 
+# run the application
 app = App(backend=kannel, sender_args=["user", "pass"])
 app.run()
 
